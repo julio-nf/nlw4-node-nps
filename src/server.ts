3480e3dd1b0daec08afb39fa738cf34ce18f4d89
Julio Novais <julio.novais@outlook.com>
2021-02-23 09:03:57 -0300

    feat: added day two lesson

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database'
+import { router } from './routes';
 
 const app = express();
 
-// http://localhost:3333/users
-app.get('/users', (req, res) => {
-  return res.json({
-    message: 'Hello World - NLW04'
-  });
-})
-
-app.post('/', (req, res) => {
-  return res.json({
-    message: 'Os dados foram salvos com sucesso!'
-  });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running...'))
